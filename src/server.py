9cdc4e1ee9b21404b7c47a648719274fe17b5b38
sunday <sun@chengcheng.info>
2015-03-07 23:35:33 +0800

    create todolist && docs.

--- src/server.py
+++ src/server.py
@@ -10,6 +10,8 @@ import os
 import logging
 
 import util
+from twisted.protocols.policies import TimeoutMixin
+import struct
 logger = logging.getLogger("pyvpn")
 
 
@@ -60,15 +62,59 @@ class TunProtocol(protocol.Protocol):
         pass
 
 
-class VPNProtocol(protocol.Protocol):
+class PackageType(object):
+    AUTH = 0
+    HEARTBEAT = 1
+    IFCONFIG = 2
+
+
+class VPNProtocol(protocol.Protocol, TimeoutMixin):
     def connectionMade(self):
-        pass
+        self._is_authed = False
+        self._is_gzip = True
+        self._is_encrypt = False
+        self._interval = 120
+        self._client_addr = None
+        self._client_mask = None
+        self._flow_count = 0
+        self.buf = ''
+        self.setTimeout(self._interval)
 
     def connectionLost(self, reason):
+        self.setTimeout(None)
+
+    def getpackage(self):
+        if len(self.buf) <= 2:
+            return None
+        d_length = struct.unpack('@H', self.buf[:2])[0]
+        if len(self.buf) < d_length:
+            return None
+        pack = self.buf[:d_length]
+        self.buf = self.buf[d_length:]
+        return pack
+
+    def user_auth(self, user, pwd):
         pass
 
     def dataReceived(self, data):
-        pass
+        self.setTimeout(None)
+        self.buf += data
+        self._flow_count += len(data)
+        while True:
+            pack = self.getpackage()
+            if not pack:
+                break
+            pack_type = struct.unpack('@b', pack[2])[0]
+            if pack_type == PackageType.AUTH:
+                user_len = struct.unpack('@b', pack[3])[0]
+                assert user_len
+                user = pack[4: 4 + user_len]
+                pwd_len = struct.unpack('@b', pack[4 + user_len])[0]
+                assert pwd_len
+                pwd = pack[4 + user_len + 1:]
+                self.user_auth(user, pwd)
+            elif pack_type == PackageType.HEARTBEAT:
+                pass
 
 
 def main():
@@ -80,3 +126,18 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# TODO:
+r"""
+1, VPN
+1.1, 自动的IP配置
+2, Win32 client
+2.1， Android client
+3, 简单的认证机制，地址端口用户名密码
+4, 压缩与可选的加密机制
+5, 流量控制
+6, 限速
+7, Web控制与状态显示
+LAST, 性能
+"""
